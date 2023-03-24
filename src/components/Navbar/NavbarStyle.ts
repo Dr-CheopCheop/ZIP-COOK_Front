54e62dc17dab83ed1575f7bb158a7b68e4776be5
youngduck <dudejr5839@naver.com>
2023-03-25 01:08:14 +0900

    feat: Add Navbar

---
+++ src/components/Navbar/NavbarStyle.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px 12px;
+`;
+
+export const Ul = styled.ul`
+  width: 100%;
+  list-style: none;
+  display: flex;
+`;
+
+export const Logo = styled.div`
+  font-size: 30px;
+`;
+
+export const Li = styled.li`
+  font-size: 20px;
+`;
