b8f464a6360daf92d03239d88ae6c9468083004c
Yeon su <dustn2366@naver.com>
2023-03-28 18:32:50 +0900

    Loginpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { GlobalStyle } from "./components/Styles/GlobalStyle";
 import Navbar from "./components/Navbar/Navbar";
+import Login from "./components/Login/Login";
+
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Navbar />
+      <Login />
     </>
   );
 }
