591953ac1b601c67216a1935b2b9cc0dc3921ee2
youngduck <dudejr5839@naver.com>
2023-03-30 00:08:07 +0900

    feat: Add Discount component

--- src/Pages/Community/Community.tsx
+++ src/Pages/Community/Community.tsx
@@ -1,10 +1,22 @@
 import Navbar from "../../components/Navbar/Navbar";
+import { Link } from "react-router-dom";
 
 const Community = () => {
   return (
     <div>
       <Navbar />
       <h1>통합 커뮤니티 페이지</h1>
+      <ul>
+        <li>
+          <Link to="/community/recipe">레시피</Link>
+        </li>
+        <li>
+          <Link to="/community/share">공유</Link>
+        </li>
+        <li>
+          <Link to="/community/discount">할인정보</Link>
+        </li>
+      </ul>
     </div>
   );
 };
