9f5092f01a7bfc1f40822308cdf7cf0c88dbf5f4
youngduck <dudejr5839@naver.com>
2023-04-01 22:46:42 +0900

    feat: Add style

--- src/components/Styles/Frames.ts
+++ src/components/Styles/Frames.ts
@@ -1,5 +1,19 @@
+import { css } from "styled-components";
+import Colors from "./Colors";
+
+const DiscountFormBox = css`
+  display: block;
+  border-radius: 10px;
+  border: 3px solid black;
+  width: 800px;
+  height: 80px;
+  font-size: 20px;
+  background-color: ${Colors.postBackground};
+`;
+
 const Frames = {
-  navbar_height: "40px",
+  navbar_height: "60px",
+  DiscountFormBox,
 };
 
 export default Frames;
