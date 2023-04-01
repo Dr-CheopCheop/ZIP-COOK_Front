e65880f6ff6c28c6db56d04beabeb4c0b7eed1fd
youngduck <dudejr5839@naver.com>
2023-04-01 22:29:50 +0900

    feat: Add Discount comonent #2

--- src/components/Discount/DiscountForm.tsx
+++ src/components/Discount/DiscountForm.tsx
@@ -1,29 +1,74 @@
-import { useForm, SubmitHandler } from "react-hook-form";
 import * as S from "./DiscountFormStyle";
+
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useEffect, useState } from "react";
 
 interface FormValue {
   title: string;
   price: string;
+  region: string;
   img: FileList;
 }
 const DiscountForm = () => {
-  const { register, handleSubmit } = useForm<FormValue>();
+  const [imagePreview, setImagePreview] = useState<any>("");
+  const { register, handleSubmit, watch } = useForm<FormValue>();
+  const image = watch("img");
+
+  useEffect(() => {
+    if (image && image.length > 0) {
+      const file = image[0];
+      setImagePreview(URL.createObjectURL(file));
+    }
+  }, [image]);
 
   const onSubmitHandler: SubmitHandler<FormValue> = (data) => {
-    console.log(data);
+    const formData = new FormData();
+    formData.append("price", data.price);
+    formData.append("title", data.title);
+    formData.append("region", data.region);
+    formData.append("image", data.img[0]);
+
+    // formdata 콘솔확인용
+    for (let key of formData.values()) {
+      console.log(key);
+    }
   };
 
   return (
     <S.Form onSubmit={handleSubmit(onSubmitHandler)}>
-      <S.Input {...register("title", { required: true })} />
-      <S.Input {...register("price", { required: true })} />
-      <S.Input
-        {...register("img", { required: true })}
-        type="file"
-        accept="image/*"
-      />
+      <S.Container>
+        <S.Manual>
+          <p>할인 정보를 입력해 주세요.</p>
+        </S.Manual>
+        <label>
+          <S.ImgView>
+            {imagePreview ? (
+              <img src={imagePreview} alt="" />
+            ) : (
+              <p>사진 첨부</p>
+            )}
+          </S.ImgView>
+          <S.ImgInput
+            {...register("img", { required: true })}
+            type="file"
+            accept="image/*"
+          />
+        </label>
+        <S.Input
+          placeholder="할인 상품명"
+          {...register("title", { required: true })}
+        />
+        <S.Input
+          placeholder="할인 상품 가격"
+          {...register("price", { required: true })}
+        />
+        <S.Input
+          placeholder="지역"
+          {...register("region", { required: true })}
+        />
 
-      <S.Button>완료</S.Button>
+        <S.Button>완료</S.Button>
+      </S.Container>
     </S.Form>
   );
 };
--- src/components/Discount/DiscountFormStyle.ts
+++ src/components/Discount/DiscountFormStyle.ts
@@ -1,19 +1,63 @@
 import styled from "styled-components";
 import Colors from "../Styles/Colors";
+import Frames from "../Styles/Frames";
 
 export const Form = styled.form`
-  border-top: 1px solid black;
-  width: 80%;
-  margin: 70px auto;
+  border-top: 2px solid rgba(0, 0, 0, 0.5);
+  width: 900px;
+  margin: 20px auto;
+`;
+
+export const Container = styled.div`
+  margin: 0px auto;
+  padding: 20px 0;
+  text-align: center;
+`;
+
+export const Manual = styled.div`
+  ${Frames.DiscountFormBox};
+  margin: 0 auto;
+  line-height: 80px;
+  background-color: white;
+  color: rgba(0, 0, 0, 0.5);
+  border: none;
 `;
 
 export const Input = styled.input`
-  display: block;
-  margin: 0 auto;
+  ${Frames.DiscountFormBox};
+
+  line-height: 80px;
+  margin: 20px auto;
+  text-align: center;
+  border: none;
+  background-color: ${Colors.postBackground};
+`;
+
+export const ImgInput = styled(Input)`
+  display: none;
+`;
+
+export const ImgView = styled.div`
+  ${Frames.DiscountFormBox};
+  background-color: white;
+  height: 300px;
+  line-height: 300px;
+  margin: 10px auto;
+  color: rgba(0, 0, 0, 0.3);
+  border: 3px solid ${Colors.postBackground};
+  cursor: pointer;
+  &:hover {
+    border: 3px solid ${Colors.postBackgroundBorder};
+  }
 `;
 
 export const Button = styled.button`
   display: block;
   margin: 0 auto;
   background-color: ${Colors.zipCookYellow};
+  border-radius: 10px;
+  border: none;
+  width: 100px;
+  height: 40px;
+  font-weight: 700;
 `;
