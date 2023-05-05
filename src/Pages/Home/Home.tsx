e4d0a6554758e4c4e882e55dce4c24b0d1917ae9
jiheeyomi <68063103+jiheeyomi@users.noreply.github.com>
2023-05-05 20:25:23 +0900

    feat: add MainPage

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import HomePage from "../../components/Home/Home";
 import Navbar from "../../components/Navbar/Navbar";
-
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
---
+++ src/components/Main/Pagination.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import * as M from './MainStyle';
+
+const Pagination = ({ postsPerPage, totalPosts, paginate }: any ) => {
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+    return (
+        <div>
+            <nav>
+                <M.PageButtonWrap>
+                    {pageNumbers.map((number) => (
+                        <M.PageButton onClick={() => paginate(number)}>
+                            {number}
+                        </M.PageButton>
+                    ))}
+                </M.PageButtonWrap>
+            </nav>
+        </div>
+    )
+};
+
+export default Pagination;
