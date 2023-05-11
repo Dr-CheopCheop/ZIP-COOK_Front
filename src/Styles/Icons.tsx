f5ebe6ea5294c5bb8a29b43614da8c97fec2383c
youngduck <dudejr5839@naver.com>
2023-05-11 14:24:38 +0900

    add: react-icons dicrection

--- src/Styles/Icons.tsx
+++ src/Styles/Icons.tsx
@@ -1,6 +1,7 @@
 import { BsCamera, BsSearch } from "react-icons/bs";
 import { GiCook } from "react-icons/gi";
 import { BiError } from "react-icons/bi";
+import { WiDirectionDown } from "react-icons/wi";
 import { CgSmartHomeRefrigerator } from "react-icons/cg";
 const Icons = {
   camera: <BsCamera />,
@@ -8,6 +9,7 @@ const Icons = {
   chef: <GiCook />,
   error: <BiError />,
   refrigerator: <CgSmartHomeRefrigerator />,
+  downDirection: <WiDirectionDown />,
 };
 
 export default Icons;
