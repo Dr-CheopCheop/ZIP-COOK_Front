bb3815d5f4cb635e4807777fa76390a01783fe60
youngduck <dudejr5839@naver.com>
2023-06-02 09:44:10 +0900

    fix: interfaces

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,22 +1,22 @@
 import * as L from "./LoginStyle";
 import icon from "../../img/Gb.jpeg";
-import React,{useState} from 'react';
-import {useForm} from "react-hook-form";
-import { useDispatch } from 'react-redux';
-import {  useNavigate } from "react-router-dom";
-import {loginUser} from "../../reducer/userSlice"
-import axios from 'axios';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../../reducer/userSlice";
+import axios from "axios";
 import { SET_TOKEN } from "../../reducer/tokenSlice";
 
 interface FormValue {
-  username : string
-  password : string
+  username: string;
+  password: string;
 }
 
 const Loginpage = () => {
   const dispatch = useDispatch();
   const movepage = useNavigate();
-  const [isLoading,setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [meg, setMsg] = useState("");
 
   function JoinPage() {
@@ -37,37 +37,40 @@ const Loginpage = () => {
     formState: { errors },
   } = useForm<FormValue>();
 
-  const onSubmit = async (data:FormValue) => {
+  const onSubmit = async (data: FormValue) => {
     setIsLoading(true);
     //Loading 메세지 출력
     setMsg("Loding...");
 
     //API
-    try{
-      const response = await axios.post("http://localhost:8080/auth/login", data,{
-        headers : {
-          "Content-Type": "application/json"
-        },
-      });
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/auth/login",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
       const user = response.data;
       const jwtToken = user.token;
       //token 저장
-      localStorage.setItem("accessToken",jwtToken);
-      axios.defaults.headers.common['Authorization'] = `Bearer ${jwtToken}`;
-      axios.get('http://localhost:8080/auth/user')
-      .then(res => {
+      localStorage.setItem("accessToken", jwtToken);
+      axios.defaults.headers.common["Authorization"] = `Bearer ${jwtToken}`;
+      axios.get("http://localhost:8080/auth/user").then((res) => {
         console.log(res);
         const userInfo = res.data;
         dispatch(loginUser(userInfo));
-      })
+      });
       dispatch(SET_TOKEN(jwtToken));
       dispatch(loginUser(user));
-      movepage('/main');
-    }catch(error) {
-      console.log('login fail', error);
-    }  
-  }
- 
+      movepage("/main");
+    } catch (error) {
+      console.log("login fail", error);
+    }
+  };
+
   return (
     <L.Container>
       <L.Test to={"/main"}>
@@ -75,28 +78,38 @@ const Loginpage = () => {
       </L.Test>
       <div>
         <form onSubmit={handleSubmit(onSubmit)}>
-        <L.Sign>
-          <L.Signinput
+          <L.Sign>
+            <L.Signinput
               type="text"
               placeholder="username"
-              {...register("username", { required: true})}
+              {...register("username", { required: true })}
             />
             {errors.username && <span>username를 입력해주세요!</span>}
             {errors.username && errors.username.type === "maxLength" && (
               <div>username는 8글자 이내로 작성해주세요.</div>
             )}
-          <L.Signinput type="password" placeholder="PassWord" {...register("password", {required:true,
-          minLength: 8,
-          pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
-          })}/>
-          {errors.password && errors.password.type === "minLength" && (
-            <div>8글자 이상 입력해주세요</div>
-          )}
-          {errors.password && errors.password.type === "pattern" && (
-                <div>비밀번호는 영문 대/소문자, 숫자, 특수문자를 포함해 입력해주세요.</div>
-          )}
-          <L.Loginbutton type="submit" disabled={isLoading}>{isLoading ? "LOGING..." : "LOGIN"}</L.Loginbutton>
-        </L.Sign>
+            <L.Signinput
+              type="password"
+              placeholder="PassWord"
+              {...register("password", {
+                required: true,
+                minLength: 8,
+                pattern:
+                  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+              })}
+            />
+            {errors.password && errors.password.type === "minLength" && (
+              <div>8글자 이상 입력해주세요</div>
+            )}
+            {errors.password && errors.password.type === "pattern" && (
+              <div>
+                비밀번호는 영문 대/소문자, 숫자, 특수문자를 포함해 입력해주세요.
+              </div>
+            )}
+            <L.Loginbutton type="submit" disabled={isLoading}>
+              {isLoading ? "LOGING..." : "LOGIN"}
+            </L.Loginbutton>
+          </L.Sign>
         </form>
         <div>
           <L.button onClick={ForgotPage}>ID/PassWord Forgot</L.button>
@@ -105,7 +118,7 @@ const Loginpage = () => {
         </div>
         <L.Line></L.Line>
         <L.Div>
-        <L.Googleb type="button" onClick={Googlepage} >
+          <L.Googleb type="button" onClick={Googlepage}>
             <L.img src={icon} />
           </L.Googleb>
         </L.Div>
